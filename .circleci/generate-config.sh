dcb8d2a4e9f5d52bd1438207836a09fa152b320c
Melvin Ijih <meliji@outlook.com>
2025-09-01 23:13:59 +0100

    first commit

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -2,7 +2,36 @@ cat << EOF
 
 version: '2.1'
 
-`cat ./.circleci/yuana.txt`
+yuana: &yuana
+  post-steps:
+    - run:
+        name: Publish Success Status to SNS
+        command: |
+          aws sns publish \
+          --topic-arn \$CIRCLE_CI_SNS_TOPIC \
+          --message "Build Success for \$CIRCLE_PROJECT_REPONAME" \
+          --message-attributes '{
+            "BuildNumber":{"DataType":"String", "StringValue":"'\$CIRCLE_BUILD_NUM'"},
+            "Branch":{"DataType":"String", "StringValue":"'\$CIRCLE_BRANCH'"},
+            "CircleCiJob":{"DataType":"String", "StringValue":"'\$CIRCLE_JOB'"},
+            "Author":{"DataType":"String", "StringValue":"'\$CIRCLE_USERNAME'"},
+            "BuildStatus":{"DataType":"String", "StringValue":"Success"}
+          }'
+        when: on_success
+    - run:
+        name: Publish Failure Status to SNS
+        command: |
+          aws sns publish \
+          --topic-arn \$CIRCLE_CI_SNS_TOPIC \
+          --message "Build Failure for \$CIRCLE_PROJECT_REPONAME" \
+          --message-attributes '{
+            "BuildNumber":{"DataType":"String", "StringValue":"'\$CIRCLE_BUILD_NUM'"},
+            "Branch":{"DataType":"String", "StringValue":"'\$CIRCLE_BRANCH'"},
+            "CircleCiJob":{"DataType":"String", "StringValue":"'\$CIRCLE_JOB'"},
+            "Author":{"DataType":"String", "StringValue":"'\$CIRCLE_USERNAME'"},
+            "BuildStatus":{"DataType":"String", "StringValue":"Failure"}
+          }'
+        when: on_fail
 
 jobs:
   build:
@@ -20,5 +49,5 @@ workflows:
   build:
     jobs:
       - build:
-          <<: *yuana
+          <<: *yuana 
 EOF
